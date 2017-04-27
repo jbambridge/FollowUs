a24c79d5a7470f456c7d416e9fb5525907dedbc4
John Bambridge <jbambridge@imac.local>
2017-04-27 11:41:53 +0200

    Initial Commit

---
+++ FollowUs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FollowUs
+//
+//  Created by John Bambridge on 27/04/2017.
+//  Copyright © 2017 LuBridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
